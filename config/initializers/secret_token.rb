fe471aee3b1264fe002d56072418812cd8de22f9
Rodrigo Alves <rodrigovieira1994@gmail.com>
2013-11-05 18:32:37 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Receba::Application.config.secret_key_base = 'a30f758eabde3bea5d9dad1e5571368c3a0f996fb938569820019ecca4f662914d9f9db3ffa5d1f1abc63e9fb0c5fd65048e14c7cb075823bcb978e63018c8d7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Receba::Application.config.session_store :cookie_store, key: '_receba_session'
