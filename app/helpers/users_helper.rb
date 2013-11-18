0a683f20837670d76adca2889a8fba2187cdac41
Rodrigo Alves <rodrigovieira1994@gmail.com>
2013-11-18 19:54:34 -0300

    Improving layout by using bigger user avatar and centering user card

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
   def show_user_avatar(user)
-    "https://graph.facebook.com/#{user.uid}/picture?width=140&height=140"
+    "https://graph.facebook.com/#{user.uid}/picture?width=480&height=480"
   end
 end
