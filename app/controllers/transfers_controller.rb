cb26331cfc8b88bfcc3dc68da073127f95cf5ff1
Rodrigo Alves <rodrigovieira1994@gmail.com>
2013-12-02 17:50:32 -0300

    Starting Moip integration

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -13,6 +13,56 @@ class TransfersController < ApplicationController
   end
 
   def create
+    payer = MyMoip::Payer.new(
+      id:                    transfer_params[:receiver_id],
+      name:                  transfer_params[:name],
+      email:                 transfer_params[:email]
+    )
+
+    credit_card = MyMoip::CreditCard.new(
+      logo:                     'visa',
+      card_number:              '4916654211627608',
+      expiration_date:          '06/15',
+      security_code:            '000',
+      owner_name:               'Juquinha da Rocha',
+      owner_birthday:           '03/11/1984',
+      owner_phone:              '5130405060',
+      owner_cpf:                '52211670695',
+    )
+
+    # Percentage value
+    commissions = MyMoip::Commission.new(
+      reason: 'Shipping',
+      receiver_login: 'commissioned_moip_login',
+      percentage_value: 0.01
+    )
+
+    instruction = MyMoip::Instruction.new(
+      id:             'instruction_id_defined_by_you',
+      payment_reason: 'Order in Buy Everything Store',
+      values:         [100.0],
+      commissions:    commissions,
+      payer:          payer
+    )
+
+    credit_card = MyMoip::CreditCard.new(
+      logo:                     'visa',
+      card_number:              '4916654211627608',
+      expiration_date:          '06/15',
+      security_code:            '000',
+      owner_name:               'Juquinha da Rocha',
+      owner_birthday:           '03/11/1984',
+      owner_phone:              '5130405060',
+      owner_cpf:                '52211670695',
+    )
+
+    transparent_request = MyMoip::TransparentRequest.new('your_logging_id')
+    transparent_request.api_call(instruction)
+
+    credit_card_payment = MyMoip::CreditCardPayment.new(credit_card, installments: 1)
+    payment_request     = MyMoip::PaymentRequest.new('your_logging_id')
+    payment_request.api_call(credit_card_payment, token: transparent_request.token)
+
     @transfer = Transfer.new(transfer_params)
     if @transfer.save
       redirect_to @transfer, notice: 'Pagamento realizado com sucesso!'
---
+++ config/initializers/my_moip.rb
@@ -0,0 +1,7 @@
+MyMoip.environment = 'production' # 'sandbox' by default
+
+MyMoip.sandbox_token    = 'your_moip_sandbox_token'
+MyMoip.sandbox_key      = 'your_moip_sandbox_key'
+
+MyMoip.production_token = 'your_moip_production_token'
+MyMoip.production_key   = 'your_moip_production_key'
